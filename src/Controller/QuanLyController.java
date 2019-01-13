c200bbc45e1901380c79dcf6fc44d17a590875d4
lam <hieuruoixanhxalo@gmail.com>
2019-01-14 00:22:53 +0700

    Update Date and Start & Finish

--- src/Controller/QuanLyController.java
+++ src/Controller/QuanLyController.java
@@ -6,7 +6,9 @@
 package Controller;
 
 import Model.QuanLyModel;
+import static java.lang.Thread.sleep;
 import java.net.URL;
+import java.sql.Time;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.ResourceBundle;
@@ -14,10 +16,13 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.beans.value.WeakChangeListener;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -30,47 +35,57 @@ import javafx.scene.layout.StackPane;
  * @author Administrator
  */
 public class QuanLyController implements Initializable {
-    
+
     private ObservableList<QuanLyModel> listProduct = FXCollections.observableArrayList();
-    
+
     @FXML
     private TableView<QuanLyModel> tableItems;
-    
+
     @FXML
     private TableColumn<QuanLyModel, Integer> idcol;
-    
+
     @FXML
     private TableColumn<QuanLyModel, String> namecol;
-    
+
     @FXML
     private TableColumn<QuanLyModel, Integer> quantitycol;
-    
+
     @FXML
     private TableColumn<QuanLyModel, Float> pricecol;
-    
+
     @FXML
     private Label labelItems;
-    
+
     @FXML
     private Label labelSales;
-    
+
     @FXML
     private Label labelUsers;
-    
+
     @FXML
     private Label labelLogout;
-    
+
     @FXML
     private StackPane SalesStack;
-    
+
     @FXML
     private StackPane ItemsStack;
-    
+
     @FXML
     private StackPane UsersStack;
     @FXML
     private Label lblTime;
-    
+    @FXML
+    private Label lblDate;
+    @FXML
+    private Button btnSalesStart;
+    @FXML
+    private Label lblSalesIntime;
+    @FXML
+    private Label lblSalesOuttime;
+    @FXML
+    private Button btnSalesFinish;
+
     void click_Sales() {
         labelSales.setDisable(true);
         labelItems.setDisable(false);
@@ -79,7 +94,7 @@ public class QuanLyController implements Initializable {
         ItemsStack.setVisible(false);
         UsersStack.setVisible(false);
     }
-    
+
     void click_Items() {
         labelSales.setDisable(false);
         labelItems.setDisable(true);
@@ -88,7 +103,7 @@ public class QuanLyController implements Initializable {
         ItemsStack.setVisible(true);
         UsersStack.setVisible(false);
     }
-    
+
     void click_Users() {
         labelSales.setDisable(false);
         labelItems.setDisable(false);
@@ -98,42 +113,60 @@ public class QuanLyController implements Initializable {
         UsersStack.setVisible(true);
     }
     
-    public void getTime() {
-        Thread timeThread = new Thread() {
-            public void run() {
-                try {
-                    while (true) {
-                        Date d = new Date();
-                        SimpleDateFormat s = new SimpleDateFormat("yyyy-MM-dd");
-                        SimpleDateFormat h = new SimpleDateFormat("HH-mm-ss");
-
-                        lblTime.setText(s.format(d)    +       h.format(d));
-//                        lblTime.textProperty().addListener(new ChangeListener<String>() {
-//                            @Override
-//                            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
-////                                System.out.println("Date : " + s.format(d) + " Time : " + h.format(d));
-//                                System.out.println("a"+newValue);
-//                                System.out.println("b"+oldValue);
-////                                lblTime.setText("Date : " + s.format(d) + " Time : " + h.format(d));
-//                            }
-//                        });
-//                        System.out.println("Date : " + s.format(d) + " Time : " + h.format(d));
-                        sleep(1000);
-                    }
-                } catch (InterruptedException ex) {
-                    Logger.getLogger(QuanLyController.class.getName()).log(Level.SEVERE, null, ex);
-                }
-            }
-        };
-        
-        timeThread.start();
+    void click_btnStartIntime() {
+        lblSalesOuttime.setText(null);
+        lblSalesIntime.setText(lblTime.getText());
     }
+    
+    void click_btnFinishOuttime() {
+        lblSalesOuttime.setText(lblTime.getText());
+    }
+
+    Task<Time> taskDate = new Task<Time>() {
+        @Override
+        protected Time call() throws Exception {
+            while (true) {
+                Date d = new Date();
+                SimpleDateFormat s = new SimpleDateFormat("yyyy-MM-dd");
+                updateMessage(s.format(d));
+                Thread.sleep(1000);
+            }
+        }
+
+        @Override
+        protected void updateMessage(String message) {
+            super.updateMessage(message);
+        }
+
+    };
+
+    Task<Time> taskTime = new Task<Time>() {
+        @Override
+        protected Time call() throws Exception {
+            while (true) {
+                Date d = new Date();
+                SimpleDateFormat h = new SimpleDateFormat("HH-mm-ss");
+                updateMessage(h.format(d));
+                Thread.sleep(1000);
+            }
+        }
+
+        @Override
+        protected void updateMessage(String message) {
+            super.updateMessage(message);
+        }
+    };
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        lblDate.textProperty().bind(taskDate.messageProperty());
+        lblTime.textProperty().bind(taskTime.messageProperty());
+        new Thread(taskDate).start();
+        new Thread(taskTime).start();
+
         idcol.setCellValueFactory(new PropertyValueFactory<>("id"));
         namecol.setCellValueFactory(new PropertyValueFactory<>("name"));
         pricecol.setCellValueFactory(new PropertyValueFactory<>("price"));
@@ -147,7 +180,7 @@ public class QuanLyController implements Initializable {
 //
         listProduct.add(model);
         tableItems.setItems(listProduct);
-        
+
         labelSales.setOnMouseClicked((event) -> {
             click_Sales();
         });
@@ -157,6 +190,13 @@ public class QuanLyController implements Initializable {
         labelUsers.setOnMouseClicked((event) -> {
             click_Users();
         });
-        getTime();
+        
+        btnSalesStart.setOnMouseClicked((event) -> {
+            click_btnStartIntime();
+        });
+        btnSalesFinish.setOnMouseClicked((event) -> {
+            click_btnFinishOuttime();
+        });
     }
+
 }
